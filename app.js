2ac636db280e5adef445bada53921f4ca60849b6
Kumail Naqvi <kumailmn@gmail.com>
2018-01-01 01:10:22 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.listen(port, function(){
+  console.log("Server started on port " + port);
+});
