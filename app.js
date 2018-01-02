0f4dd70fefe7c2b653ce4962157bbf8eefd3a5d5
Kumail Naqvi <kumailmn@gmail.com>
2018-01-01 20:12:39 -0500

    add favicon

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require("express");
+var favicon = require('serve-favicon');
 var app = express();
 
 var port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 //app.use(express.static(__dirname + "/public"));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.get("/", function(req, res){
   res.render("home");
