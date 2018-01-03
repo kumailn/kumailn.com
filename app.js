3478429c92619feccf28ae8d9ed26f9a73e0996f
Kumail Naqvi <kumailmn@gmail.com>
2018-01-03 09:35:40 -0500

    update UI

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
-//app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.get("/", function(req, res){
